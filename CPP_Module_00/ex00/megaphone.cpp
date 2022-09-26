639b49ff49036bc0ac82c945a9526b225ef35d97
Haseeb Qureshie <hqureshi@f1r6s15.codam.nl>
2022-09-26 17:05:04 +0200

    Module_00 ex_00

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void	megaphone(int argc, char **argv)
+{
+	int	i = 1;
+	int	j;
+
+	while (argv[i] && argc > 0)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				argv[i][j] = toupper(argv[i][j]);
+			std::cout << argv[i][j];
+			j++;
+		}
+		i++;
+		argc--;
+	}
+}
+
+int	main(int argc, char **argv) {
+
+	if (argc == 1)
+		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+		megaphone(argc, argv);
+	return (0);
+}
