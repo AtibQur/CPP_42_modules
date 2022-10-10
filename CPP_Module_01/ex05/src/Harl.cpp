d28b609c45d8f2879141a668282ac8d6694e5205
Haseeb Qureshie <hqureshi@f1r6s15.codam.nl>
2022-10-10 11:50:27 +0200

    CPP01 ex05 pointerfunction changes

--- CPP_Module_01/ex05/src/Harl.cpp
+++ CPP_Module_01/ex05/src/Harl.cpp
@@ -7,12 +7,16 @@ Harl::~Harl() {}
 void    Harl::complain(std::string level) {
     const int cLevel = 4;
 
-    std::string ComplainLevel[cLevel] 
-    = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    std::string ComplainLevel[cLevel] = {
+        "DEBUG", "INFO", "WARNING", "ERROR"
+    };
 
-    func_ptr ArrayOfLevels[cLevel]
-    = {
+    //void (Harll::*func_ptr(cLevel))
+    func_ptr ArrayOfLevels[cLevel] = {
         &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
     };
 
     for (int i = 0; i < cLevel; i++) {
@@ -21,11 +25,25 @@ void    Harl::complain(std::string level) {
             return ;
         }
     }
-    std::cerr << "Wrong input, try again." << std::endl;
-
+    std::cerr << "\033[1;31mWrong input.\033[0m\nTry: DEBUG, INFO, WARNING, ERROR" << std::endl;
 }
 
-void debug(void) {
-    std::cout << "DEBUG LEVEL:" << std::endl;
+void Harl::debug(void) {
+    std::cout << "\033[1;32mDEBUG:\033[0m" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void) {
+    std::cout << "\033[1;32mINFO:\033[0m" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void) {
+    std::cout << "\033[1;32mWARNING:\033[0m" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void) {
+    std::cout << "\033[1;32mERROR:\033[0m" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
--- CPP_Module_01/ex05/src/main.cpp
+++ CPP_Module_01/ex05/src/main.cpp
@@ -3,12 +3,10 @@
 int	main(int argc, char **argv) {
 	Harl *harl = new Harl;
 
-	if (argc == 2)
-		for (int i = 1; i < argc; i++) {
+	for (int i = 1; i < argc; i++) {
 		if (argc > 1)
 			harl->complain(argv[i]);
 	}
-
 	delete harl;
 	return (0);
 }
