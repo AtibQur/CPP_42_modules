359a0693dcb17242ea445186fcc93ca4d25917c2
Haseeb Qureshie <hqureshi@f1r6s17.codam.nl>
2022-10-21 14:29:38 +0200

    CPP03 ex00 small changes/finished

--- CPP_Module_03/ex00/src/ClapTrap.cpp
+++ CPP_Module_03/ex00/src/ClapTrap.cpp
@@ -17,16 +17,16 @@ ClapTrap::ClapTrap(std::string name) :
     hitpoints(10),
     energypoints(10),
     attackdmg(0) {
-    std::cout << "Claptrap " << this->name << " has been created." << std::endl;
+    std::cout << "ClapTrap " << this->name << " has been created." << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& other) {
     *this = other;
-    std::cout << "Claptrap " << this->name << " COPY has been created" << std::endl;
+    std::cout << "ClapTrap " << this->name << " COPY has been created." << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
-    std::cout << "Destructor called" << std::endl;
+    std::cout << "Destructor called." << std::endl;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
@@ -34,7 +34,7 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
     this->hitpoints = other.hitpoints;
     this->energypoints = other.energypoints;
     this->attackdmg = other.attackdmg;
-    std::cout << "Claptrap " << this->name << " has been created and assigned" << std::endl;
+    std::cout << "ClapTrap " << this->name << " has been created and ASSIGNED." << std::endl;
     return *this;
 }
 
@@ -44,38 +44,38 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
 
 void    ClapTrap::attack(const std::string& target) {
     if (this->energypoints <= 0) {
-        std::cout << "Claptrap " << this->name << " is not able to attack due to lack of energy points." << std::endl;
+        std::cout << "ClapTrap " << this->name << " is not able to attack due to lack of energy points." << std::endl;
         return ;
     }
     if (this->hitpoints <= 0) {
-        std::cout << "Claptrap " << this->name << " is not able to attack due to lack of hitpoints." << std::endl;
+        std::cout << "ClapTrap " << this->name << " is not able to attack due to lack of hitpoints." << std::endl;
         return ;
     }
     this->energypoints--;
-    std::cout << "Claptrap " << this->name << " attacks " << target << ", causing " << this->attackdmg << " points of damage!" << std::endl;
+    std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->attackdmg << " points of damage!" << std::endl;
 }
 
 void    ClapTrap::takeDamage(unsigned int amount) {
     if (this->hitpoints <= 0) {
-        std::cout << "Claptrap " << this->name << " is already dead." << std::endl;
+        std::cout << "ClapTrap " << this->name << " is already dead." << std::endl;
         return ;
     }
 
     if (amount > this->hitpoints)
         amount = this->hitpoints;
     this->hitpoints -= amount;
-    std::cout << "Claptrap " << this->name << " has been damaged for " << amount << " damage and has " << this->hitpoints << " hitpoints left." << std::endl;
+    std::cout << "ClapTrap " << this->name << " has been damaged for " << amount << " damage and has " << this->hitpoints << " hitpoints left." << std::endl;
 }
 
 void    ClapTrap::beRepaired(unsigned int amount) {
     if (this->energypoints <= 0) {
-        std::cout << "Claptrap " << this->name << " is not able to repair due to lack of energy points." << std::endl;
+        std::cout << "ClapTrap " << this->name << " is not able to repair due to lack of energy points." << std::endl;
         return ;
     }
     if (this->hitpoints <= 0) {
-        std::cout << "Claptrap " << this->name << " is not able to repair due to lack of hitpoints." << std::endl;
+        std::cout << "ClapTrap " << this->name << " is not able to repair due to lack of hitpoints." << std::endl;
         return ;
     }
     this->hitpoints += amount;
-    std::cout << "Claptrap " << this->name << " repaired itself for " << amount << " hitpoints. " << this->name << " has " << this->hitpoints << " HP." << std::endl;
+    std::cout << "ClapTrap " << this->name << " repaired itself for " << amount << " hitpoints. " << this->name << " has " << this->hitpoints << " HP." << std::endl;
 }
--- CPP_Module_03/ex00/src/main.cpp
+++ CPP_Module_03/ex00/src/main.cpp
@@ -1,11 +1,33 @@
 #include "../inc/ClapTrap.hpp"
 
-
-
 int main(void) {
+    ClapTrap *Claptrap1 = new ClapTrap("Mar");
+    ClapTrap *Claptrap2 = new ClapTrap("Sab");
+    ClapTrap *Claptrap3 = new ClapTrap(*Claptrap1);
 
-    standardTest();
+    std::cout << std::endl;
 
+    Claptrap1->attack("John");
+    Claptrap2->attack("Peter");
+    Claptrap3->attack("Bas");
+
+    std::cout << std::endl;
+
+    Claptrap1->takeDamage(5);
+    Claptrap2->takeDamage(3);
+    Claptrap3->takeDamage(6);
+
+    std::cout << std::endl;
+
+    Claptrap1->beRepaired(5);
+    Claptrap2->beRepaired(10);
+    Claptrap3->beRepaired(15);
+
+    std::cout << std::endl;
+
+    delete Claptrap1;
+    delete Claptrap2;
+    delete Claptrap3;
 
     return 0;
 }
