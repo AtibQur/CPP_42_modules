441ada2dba331d7e6d167fcb886d31e81c091e68
Haseeb Qureshie <hqureshi@f1r2s13.codam.nl>
2023-02-10 15:06:08 +0100

    Finished CPP07 ex02

--- CPP_Module_07/ex02/inc/Array.hpp
+++ CPP_Module_07/ex02/inc/Array.hpp
@@ -1,21 +1,58 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+#include <iostream>
+
 template<typename T>
 class Array
 {
     private:
         T*           elements;
-        unsigned int size;
+        unsigned int _size;
 
     public:
-        Array() : size(0), elements(nullptr) {}
+        Array() : _size(0), elements(NULL) {
+            std::cout << "Empty array created with size: " << _size << std::endl;
+        }
 
-        Array(unsigned int n) : size(n) {
+        Array(unsigned int n) : _size(n) {
+            std::cout << "Array created with size: " << _size << std::endl;
             elements = new T[n];
         }
 
-        ~Array();
+        Array(const Array& arr) : _size(arr._size) {
+            elements = new T[_size];
+            for (unsigned int i = 0; i < _size; i++) {
+                elements[i] = arr.elements[i];
+            }
+        }
+
+        Array& operator=(const Array& arr) {
+            if (this == arr) {
+                delete[] elements;
+                _size = arr._size;
+                elements = new T[_size];
+                for (unsigned int i = 0; i < _size; i++) {
+                    elements[i] = arr.elements[i];
+                }
+            }
+            return *this;
+        }
+
+        ~Array() {
+            delete[] elements; 
+        }
+
+        T& operator[](unsigned int i) {
+            if (i >= _size)
+                throw std::out_of_range("Index out of bound");
+            return elements[i];
+        }
+
+        unsigned int size() const {
+            return _size;
+        }
 };
 
 #endif
--- CPP_Module_07/ex02/src/main.cpp
+++ CPP_Module_07/ex02/src/main.cpp
@@ -1,55 +1,98 @@
 #include "../inc/Array.hpp"
 #include <iostream>
 
-#define MAX_VAL 750
+void	check_leaks() {
+	system ("leaks -q array");
+}
+
 int main(int, char**)
 {
-    Array<int> numbers(MAX_VAL);
+    atexit(check_leaks);
 
-    int * a = new int();
-    // int* mirror = new int[MAX_VAL];
-    // srand(time(NULL));
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     const int value = rand();
-    //     numbers[i] = value;
-    //     mirror[i] = value;
-    // }
-    // //SCOPE
-    // {
-    //     Array<int> tmp = numbers;
-    //     Array<int> test(tmp);
-    // }
+    int size = 5;
+    Array<int> arrInt(size);
+    Array<char> arrChar(size);
+    Array<std::string> arrString(size);
 
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     if (mirror[i] != numbers[i])
-    //     {
-    //         std::cerr << "didn't save the same value!!" << std::endl;
-    //         return 1;
-    //     }
-    // }
-    // try
-    // {
-    //     numbers[-2] = 0;
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
-    // try
-    // {
-    //     numbers[MAX_VAL] = 0;
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
+    arrInt[0] = 1;
+    arrInt[1] = 2;
+    arrInt[2] = 3;
+    arrInt[3] = 4;
+    arrInt[4] = 5;
 
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     numbers[i] = rand();
-    // }
-    // delete [] mirror;//
+    arrChar[0] = 'a';
+    arrChar[1] = 'b';
+    arrChar[2] = 'c';
+    arrChar[3] = 'd';
+    arrChar[4] = 'e';
+
+    arrString[0] = "Hello";
+    arrString[1] = "Hola";
+    arrString[2] = "Bonjour";
+    arrString[3] = "Annyeong haseyo";
+    arrString[4] = "Ohayou";
+
+    std::cout << "========== BASIC TEST ===========" << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << "Array int: " << arrInt[i] << std::endl;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << "Array char: " << arrChar[i] << std::endl;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << "Array string: " << arrString[i] << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "ArrInt size:    " << arrInt.size() << std::endl;
+    std::cout << "ArrChar size:   " << arrChar.size() << std::endl;
+    std::cout << "ArrString size: " << arrString.size() << std::endl;
+    std::cout << std::endl;
+    std::cout << "=================================" << std::endl;
+    std::cout << std::endl;
+    std::cout << "========== COPY TEST ============" << std::endl;
+    int* mirror = new int[size];
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        const int value = rand();
+        arrInt[i] = value;
+        mirror[i] = value;
+    }
+
+    for (int i = 0; i < size; i++) {
+        std::cout << "· Array  int: " << arrInt[i] << std::endl;
+        std::cout << "  Mirror int: " << mirror[i] << std::endl;
+    }
+    std::cout << "==================================" << std::endl;
+    std::cout << std::endl;
+    std::cout << "========= EXCEPTION TEST ===========" << std::endl;
+    try
+    {
+        std::cout << "Array char: " << arrChar[1] << std::endl;
+        arrChar[1] = 'S';
+        std::cout << "Array char: " << arrChar[1] << std::endl;
+        arrChar[-1] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << std::endl;
+    try
+    {
+        std::cout << "String char: " << arrString[2] << std::endl;
+        arrString[2] = "ALOHAAAAA";
+        std::cout << "String char: " << arrString[2] << std::endl;
+        arrString[-1] = "FAILURE";
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "=====================================" << std::endl;
+
+    delete[] mirror;
     return 0;
 }
